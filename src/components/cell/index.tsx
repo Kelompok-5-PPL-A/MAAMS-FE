6bd6375dfea317e28e318ed310eecc788d9d554e
bagas-shalahuddin <bagas.shalahuddin@ui.ac.id>
2024-02-27 21:13:42 +0700

    refactor(cell): enhance Cell component with status visualization and editability control
    - Integrate CauseStatus enum for dynamic visual feedback based on verification status, - Add
    disabled prop to optionally disable input for certain cells, - Apply conditional styling for
    distinct visual representation of different cell states.

--- src/components/cell/index.tsx
+++ src/components/cell/index.tsx
@@ -1,24 +1,51 @@
 import React from 'react'
+import { CauseStatus } from 'lib/enum' // Adjust the import path as necessary
 
 interface CellProps {
   cellName: string
   cause: string
   onChange: (value: string) => void
+  causeStatus: CauseStatus
+  disabled: boolean
+  placeholder: string
 }
+export const Cell: React.FC<CellProps> = ({
+  cellName,
+  cause,
+  onChange,
+  causeStatus,
+  disabled = false,
+  placeholder
+}) => {
+  const getOutlineClass = (status: CauseStatus) => {
+    switch (status) {
+      case CauseStatus.Incorrect:
+        return 'border-2 border-red-500'
+      case CauseStatus.CorrectNotRoot:
+        return 'border-2 border-green-500'
+      case CauseStatus.CorrectRoot:
+        return 'border-2 border-purple-500'
+      case CauseStatus.Unchecked:
+      default:
+        return 'border-2 border-black'
+    }
+  }
 
-export const Cell: React.FC<CellProps> = ({ cellName, cause, onChange }) => {
+  const outlineClass = getOutlineClass(causeStatus)
+
   return (
     <div className='flex flex-col items-center justify-center relative'>
-      <div className='relative w-fit mt-[-1.00px] font-bold text-black text-2xl leading-10 mb-2 whitespace-nowrap'>
+      <div className='relative w-full mt-[-1px] font-bold text-black text-2xl leading-10 mb-2 whitespace-nowrap'>
         {cellName}
       </div>
       <textarea
         value={cause}
-        onChange={(e) => onChange(e.target.value)}
-        rows={1}
+        onChange={(event) => onChange(event.target.value)}
+        rows={5}
         maxLength={148}
-        className='w-full h-22 text-xs resize-none flex pt-4 px-4 pb-16 items-center bg-[#ececec] border-solid border border-[#000] relative z-[1]'
-        placeholder='Isi sebab...'
+        className={`w-full text-xs resize-none items-center bg-[#ececec] border-solid border ${outlineClass} relative z-[1] my-2 py-2 px-4`}
+        placeholder={placeholder}
+        disabled={disabled}
       ></textarea>
     </div>
   )
