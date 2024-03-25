4c15ffb8d2023bfcc95a2eb1a74a8b76ea1a2f37
adlyrndi <adlyyrr12@gmail.com>
2024-03-25 17:55:02 +0700

    refactor(analisishistory component): [GREEN] refactoring sectionHistory component test

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,6 @@ module.exports = {
     }
   },
   rules: {
-    'react/display-name': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off'
--- src/__tests__/component/sectionHistory.test.tsx
+++ src/__tests__/component/sectionHistory.test.tsx
@@ -1,20 +1,46 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import Section from '../../components/sectionHistory'
+import { SectionHistoryProps } from '../../components/types/sectionHistory'
 import '@testing-library/jest-dom'
+// Mock useRouter hook
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    push: jest.fn()
+  })
+}))
 
-jest.mock('../../components/itemListHistory', () => () => <div data-testid='mock-list-item'></div>)
+describe('Section Component', () => {
+  it('renders section with items correctly', () => {
+    const items = [
+      { title: 'Item 1', timestamp: '2024-03-25', mode: 'read' },
+      { title: 'Item 2', timestamp: '2024-03-26', mode: 'write' }
+    ]
 
-describe('Section component', () => {
-  const sectionTitle = 'Test Section'
-  const sectionItems = [
-    { title: 'Item 1', timestamp: '2024-03-24', mode: 'test' },
-    { title: 'Item 2', timestamp: '2024-03-25', mode: 'test' }
-  ]
+    const { getByTestId, getByText } = render(<Section title='History' items={items} />)
 
-  it('renders list items', () => {
-    const { getAllByTestId } = render(<Section title={sectionTitle} items={sectionItems} />)
-    const listItems = getAllByTestId('mock-list-item')
-    expect(listItems.length).toBe(sectionItems.length)
+    // Ensure section title renders
+    expect(getByTestId('History-section')).toBeInTheDocument()
+
+    // Ensure each item title renders
+    expect(getByText('Item 1')).toBeInTheDocument()
+    expect(getByText('Item 2')).toBeInTheDocument()
+
+    // You can add more assertions for timestamps, modes, etc.
+  })
+
+  it('renders section with no items when items array is empty', () => {
+    const items: SectionHistoryProps['items'] = [] // empty array
+
+    const { getByTestId, queryByText } = render(<Section title='History' items={items} />)
+
+    // Ensure section title renders
+    expect(getByTestId('History-section')).toBeInTheDocument()
+
+    // Ensure no item is rendered
+    expect(queryByText('Item 1')).toBeNull()
+    expect(queryByText('Item 2')).toBeNull()
+
+    // You can add more assertions if necessary.
   })
 })
