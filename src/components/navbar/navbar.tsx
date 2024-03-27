22662210544e6732021123e1d68524614a47056c
Raditya Aditama <raditya.aditama@ui.ac.id>
2024-03-28 03:57:19 +0700

    fix(navbar): fixed useEffect hook implementation for setIsStaff

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -19,8 +19,11 @@ const Navbar = () => {
     setUserData(JSON.parse(localStorage.getItem('userData')!))
     console.log(JSON.parse(localStorage.getItem('userData')!))
     setIsLoggedIn(refresh_token === 'true')
-    setIsStaff(userData ? userData.is_staff : false)
   }, [])
+
+  useEffect(() => {
+    setIsStaff(userData?.is_staff || false)
+  }, [userData])
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
@@ -96,7 +99,7 @@ const Navbar = () => {
               <ul className='flex flex-col font-bold md:items-center md:justify-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-white md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-[#FBC707]'>
                 <li>
                   <a
-                    href='#'
+                    href='/history'
                     className='block py-2 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0'
                   >
                     Riwayat
