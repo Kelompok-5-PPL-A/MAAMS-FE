7b4900b997a3388079948d5d20b22b6f70c7ecc1
Nicholas Sidharta <toxicancm@gmail.com>
2024-03-31 01:39:40 +0700

    feat(green (edit profile)): gREEN STAGE: Edit profile page and navbar adjustment to redirect to page
    100% coverage on test case, All pass test case, working page for edit profile, and navbar adjustment
    to redirect on click edit profile button

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -145,7 +145,7 @@ const Navbar = () => {
                     >
                       <ul className='py-2 text-sm text-black'>
                         <li>
-                          <a href='#' className='block px-4 py-2 hover:bg-gray-100'>
+                          <a href='/editProfile' className='block px-4 py-2 hover:bg-gray-100'>
                             Edit Profile
                           </a>
                         </li>
