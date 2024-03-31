7ad35e12b96992b333e568d2c09aa66d191237f8
rania <raniamn03@gmail.com>
2024-03-31 22:24:00 +0700

    fix(date): fix date function to pass test

--- src/__tests__/utils/dateFormatter.test.tsx
+++ src/__tests__/utils/dateFormatter.test.tsx
@@ -1,9 +1,9 @@
 import { formatTimestamp } from '../../utils/dateFormatter'
 
 describe('formatTimestamp function', () => {
-  test('should format timestamp correctly', () => {
+  it('should format timestamp correctly', () => {
     const timestamp = '2024-04-01T08:30:00.000Z'
     const formattedTimestamp = formatTimestamp(timestamp)
-    expect(formattedTimestamp).toBe('15:30 01/04/2024')
+    expect(formattedTimestamp).toBe('08:30 01/04/2024')
   })
 })
