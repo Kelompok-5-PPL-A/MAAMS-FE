6fb32a0bda9a7ba82d1895df13de3b12fa2c874f
Nicholas Sidharta <toxicancm@gmail.com>
2024-04-29 00:09:45 +0700

    fix(section history): rEFACTOR: Fix field at title
    Change from .title to .displayed_title

--- src/components/sectionHistory/index.tsx
+++ src/components/sectionHistory/index.tsx
@@ -29,7 +29,7 @@ const Section: React.FC<
           <ListItem
             idQuestion={item.id}
             key={index}
-            title={item.title}
+            title={item.displayed_title}
             timestamp={item.timestamp}
             mode={item.mode}
             user={item.user}
