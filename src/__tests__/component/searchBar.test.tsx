093bd650126fc13b4985cd721967d4c4c7d0c5d7
rayhan randi <rayhanrandi10@gmail.com>
2024-04-28 16:55:02 +0700

    green(search-filter): adjust component tests to fit props and child component

--- src/__tests__/component/searchBar.test.tsx
+++ src/__tests__/component/searchBar.test.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
 import { SearchBar } from '../../components/searchBar'
 
 describe('SearchBar component', () => {
-  test('renders and triggers onChange and onSubmit', () => {
+  test('renders and triggers onChange and onSubmit', async () => {
+    const isAdmin = false
+    const publicAnalyses = false
+    const filter = ''
     const keyword = ''
+    const onSelect = jest.fn()
     const onChange = jest.fn()
     const onSubmit = jest.fn()
 
-    const { getByPlaceholderText } = render(<SearchBar keyword={keyword} onChange={onChange} onSubmit={onSubmit} />)
+    const { getByPlaceholderText } = render(
+      <SearchBar
+        isAdmin={isAdmin}
+        publicAnalyses={publicAnalyses}
+        filter={filter}
+        keyword={keyword}
+        onSelect={onSelect}
+        onChange={onChange}
+        onSubmit={onSubmit}
+      />
+    )
 
-    const inputElement = getByPlaceholderText('Cari analisis...')
+    const inputElement = await waitFor(() => getByPlaceholderText('Cari analisis...'))
     fireEvent.change(inputElement, { target: { value: 'test' } })
     expect(onChange).toHaveBeenCalledWith('test')
 
@@ -18,28 +32,54 @@ describe('SearchBar component', () => {
     expect(onSubmit).toHaveBeenCalled()
   })
 
-  test('does not trigger onSubmit on input change', () => {
+  test('does not trigger onSubmit on input change', async () => {
+    const isAdmin = false
+    const publicAnalyses = false
+    const filter = ''
     const keyword = ''
+    const onSelect = jest.fn()
     const onChange = jest.fn()
     const onSubmit = jest.fn()
 
-    const { getByPlaceholderText } = render(<SearchBar keyword={keyword} onChange={onChange} onSubmit={onSubmit} />)
+    const { getByPlaceholderText } = render(
+      <SearchBar
+        isAdmin={isAdmin}
+        publicAnalyses={publicAnalyses}
+        filter={filter}
+        keyword={keyword}
+        onSelect={onSelect}
+        onChange={onChange}
+        onSubmit={onSubmit}
+      />
+    )
 
-    const inputElement = getByPlaceholderText('Cari analisis...')
+    const inputElement = await waitFor(() => getByPlaceholderText('Cari analisis...'))
     fireEvent.change(inputElement, { target: { value: 'test' } })
-
     expect(onSubmit).not.toHaveBeenCalled()
   })
 
-  test('triggers onSubmit on button click after input change', () => {
-    const onSubmitMock = jest.fn()
+  test('triggers onSubmit on button click after input change', async () => {
+    const isAdmin = false
+    const publicAnalyses = false
+    const filter = ''
+    const keyword = ''
+    const onSelectMock = jest.fn()
     const onChangeMock = jest.fn()
+    const onSubmitMock = jest.fn()
 
     const { getByPlaceholderText, getByRole } = render(
-      <SearchBar keyword='' onChange={onChangeMock} onSubmit={onSubmitMock} />
+      <SearchBar
+        isAdmin={isAdmin}
+        publicAnalyses={publicAnalyses}
+        filter={filter}
+        keyword={keyword}
+        onSelect={onSelectMock}
+        onChange={onChangeMock}
+        onSubmit={onSubmitMock}
+      />
     )
 
-    const inputElement = getByPlaceholderText('Cari analisis...')
+    const inputElement = await waitFor(() => getByPlaceholderText('Cari analisis...'))
     fireEvent.change(inputElement, { target: { value: 'keyword' } })
 
     const searchButton = getByRole('button', { name: 'Search Icon' })
