e0107f8e7d13c720b5456113df5ca2376057698c
adlyrndi <adlyyrr12@gmail.com>
2024-04-29 01:22:07 +0700

    fix(analisispublik): fixinig redirect specific question

--- src/components/adminTable/index.tsx
+++ src/components/adminTable/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Item } from 'components/types/adminTable'
+import { useRouter } from 'next/router'
 
 interface Props {
   data: Item[]
 }
+
 const AdminTable: React.FC<Props> = ({ data }) => {
+  const router = useRouter()
+
+  const handleRowClick = (id: string) => {
+    router.push(`/validator/${id}`)
+  }
+
   return (
     <div className='relative overflow-x-auto shadow-md m-12'>
       <table className='w-full text-sm text-left text-black border-collapse'>
@@ -29,7 +37,11 @@ const AdminTable: React.FC<Props> = ({ data }) => {
         </thead>
         <tbody>
           {data.map(({ id, title, user, tags, timestamp }) => (
-            <tr key={id} className='bg-white hover:bg-gray-200 border border-[#FBC707] text-xs h-20'>
+            <tr
+              key={id}
+              className='bg-white hover:bg-gray-200 border border-[#FBC707] text-xs h-20 cursor-pointer'
+              onClick={() => handleRowClick(id)}
+            >
               <td className='px-6 py-4 border-b border-b-[#FBC707] truncate'>
                 {title.length > 60 ? `${title.slice(0, 60)}...` : title}
               </td>
@@ -37,20 +49,26 @@ const AdminTable: React.FC<Props> = ({ data }) => {
                 {user.length > 10 ? `${user.slice(0, 10)}...` : user}
               </td>
               <td className='px-6 py-4 flex justify-start'>
-                {tags.map((tags, tagIndex) => (
+                {tags.map((tag, tagIndex) => (
                   <button
                     key={tagIndex}
                     className='bg-[#FBC707] text-black px-3 py-3 rounded-3xl text-xs font-bold mr-3'
                   >
-                    {tags}
+                    {tag}
                   </button>
                 ))}
               </td>
               <td className='px-6 py-4 border-b border-b-[#FBC707]'>{timestamp}</td>
               <td className='px-6 py-4 text-right border-b border-b-[#FBC707]'>
-                <a href='#' className='text-blue-600 hover:underline'>
+                <span
+                  className='text-blue-600 underline cursor-pointer hover:text-sm'
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    handleRowClick(id)
+                  }}
+                >
                   Lihat
-                </a>
+                </span>
               </td>
             </tr>
           ))}
