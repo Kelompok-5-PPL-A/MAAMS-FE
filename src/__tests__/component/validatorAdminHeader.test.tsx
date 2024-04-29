7987d09d7465cb3e5a4f46f7b6fe119207f9d039
rania <raniamn03@gmail.com>
2024-04-29 20:01:52 +0700

    chore(vercel): remove vercel.json

--- src/__tests__/component/validatorAdminHeader.test.tsx
+++ src/__tests__/component/validatorAdminHeader.test.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import { ValidatorAdminHeader } from '../../components/validatorAdminHeader'
 import Mode from '../../constants/mode'
-import '@testing-library/jest-dom'
 
 const validatorData = {
   title: 'Sample Title',
@@ -36,31 +35,5 @@ describe('ValidatorAdminHeader component', () => {
     fireEvent.change(inputElement, { target: { value: 'New question' } })
 
     expect(inputElement.getAttribute('value')).toBe(validatorData.question)
-  })
-
-  it('should update the question state when input changes', () => {
-    const { getByRole } = render(
-      <ValidatorAdminHeader
-        validatorData={{
-          username: 'TestUser',
-          question: 'Question test',
-          created_at: '',
-          mode: Mode.pengawasan,
-          title: ''
-        }}
-      />
-    )
-
-    const questionInput = getByRole('textbox')
-    fireEvent.change(questionInput, { target: { value: 'New question' } })
-
-    expect(questionInput).toHaveValue('New question')
-  })
-
-  it('should render with default values when validatorData is not provided', () => {
-    const { getByRole } = render(<ValidatorAdminHeader />)
-
-    const questionInput = getByRole('textbox')
-    expect(questionInput).toHaveValue('')
   })
 })
