fc69b510a3925bc7c1d77ed32717d094bc875343
rania <raniamn03@gmail.com>
2024-04-30 12:29:44 +0700

    feat(validator): [GREEN]  Implement show tags at detail page

--- src/components/tagsGroup/index.tsx
+++ src/components/tagsGroup/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
-export const TagsGroup: React.FC<tagsGroupsProps> = () => {
-  return <div></div>
+export const TagsGroup: React.FC<tagsGroupsProps> = ({ tags = [] }) => {
+  return (
+    <div className='flex flex-wrap gap-2'>
+      {tags.map((tag, index) => (
+        <span key={index} className='h-8 p-4 bg-yellow-400 rounded-2xl justify-center items-center gap-0.5 inline-flex'>
+          {tag}
+        </span>
+      ))}
+    </div>
+  )
 }
