90a24d33f39c1171adffbec4aa4b03caceedecfe
rania <raniamn03@gmail.com>
2024-04-30 22:31:25 +0700

    refactor(validator): change tags group content to badge (reusable component)

--- src/components/tagsGroup/index.tsx
+++ src/components/tagsGroup/index.tsx
@@ -1,12 +1,11 @@
+import { Badge } from '../../components/badge'
 import React from 'react'
 
 export const TagsGroup: React.FC<tagsGroupsProps> = ({ tags = [] }) => {
   return (
     <div className='flex flex-wrap gap-2'>
       {tags.map((tag, index) => (
-        <span key={index} className='h-8 p-4 bg-yellow-400 rounded-2xl justify-center items-center gap-0.5 inline-flex'>
-          {tag}
-        </span>
+        <Badge key={index} text={tag} isRemovable={false} />
       ))}
     </div>
   )
