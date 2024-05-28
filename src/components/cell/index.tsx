53640b7a9c4b8cd0b6dcb40196ca0c3a608ff990
Nicholas Sidharta <toxicancm@gmail.com>
2024-05-28 09:17:01 +0700

    fix(cell component): uncomment blocks of code
    fix forget to uncomment code

--- src/components/cell/index.tsx
+++ src/components/cell/index.tsx
@@ -33,14 +33,14 @@ export const Cell: React.FC<CellProps> = ({
     let emoji = ''
     let color = ''
     switch (causeStatus) {
-      // case CauseStatus.CorrectRoot:
-      //   emoji = '☑️'
-      //   color = 'purple'
-      //   break
-      // case CauseStatus.CorrectNotRoot:
-      //   emoji = '✅'
-      //   color = 'green'
-      //   break
+      case CauseStatus.CorrectRoot:
+        emoji = '☑️'
+        color = 'purple'
+        break
+      case CauseStatus.CorrectNotRoot:
+        emoji = '✅'
+        color = 'green'
+        break
       case CauseStatus.Incorrect:
         emoji = '❌'
         color = 'red'
